d1787822eb1415f1e0b3a815a3495a5829f907d0
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2022-04-24 08:09:05 +0000

    sm6250-common: add custom msm_irqbalance.conf
    Unbalancing msm_drm and kgsl-3d0 reduces display latency and overall jank.

--- common.mk
+++ common.mk
@@ -334,6 +334,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf    
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
