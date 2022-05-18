ce530d3d1bcf19c11f4459692167d482c4742311
Lokesh773 <lokesh@palladiumos.com>
2022-05-18 23:10:16 +0530

    sm6250-common: Palladium

--- common.mk
+++ common.mk
@@ -119,7 +119,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap
+    GCamGOPrebuilt-V2
 
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
